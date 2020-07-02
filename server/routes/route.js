a59eb212a2e6102a29edcff484b7f72a3b913cc6
Rahul Khare <45624793+rahulkhare99@users.noreply.github.com>
2020-07-02 16:32:29 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,142 +1,144 @@
-app.get("/", function (req, res) {
-    res.render("home");
-});
+new code; can be used for changes
 
-app.get(
-    "/auth/google",
-    passport.authenticate("google", { scope: ["profile"] })
-);
+// app.get("/", function (req, res) {
+//     res.render("home");
+// });
 
-app.get(
-    "/auth/google/secrets",
-    passport.authenticate("google", { failureRedirect: "/login" }),
-    function (req, res) {
-        res.redirect("/secrets");
-    }
-);
+// app.get(
+//     "/auth/google",
+//     passport.authenticate("google", { scope: ["profile"] })
+// );
 
-app.get("/login", function (req, res) {
-    res.render("login");
-});
+// app.get(
+//     "/auth/google/secrets",
+//     passport.authenticate("google", { failureRedirect: "/login" }),
+//     function (req, res) {
+//         res.redirect("/secrets");
+//     }
+// );
 
-app.get("/register", function (req, res) {
-    res.render("register");
-});
+// app.get("/login", function (req, res) {
+//     res.render("login");
+// });
 
-app.get("/secrets", function (req, res) {
-    User.find({ secret: { $ne: null } }, function (err, foundUsers) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUsers) {
-                res.render("secrets", { userswithSecrets: foundUsers });
-            }
-        }
-    });
-});
+// app.get("/register", function (req, res) {
+//     res.render("register");
+// });
 
-app.get("/submit", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("submit");
-    } else {
-        res.redirect("/login");
-    }
-});
+// app.get("/secrets", function (req, res) {
+//     User.find({ secret: { $ne: null } }, function (err, foundUsers) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             if (foundUsers) {
+//                 res.render("secrets", { userswithSecrets: foundUsers });
+//             }
+//         }
+//     });
+// });
 
-app.post("/submit", function (req, res) {
-    const submittedSecret = req.body.secret;
-    User.findById(req.user.id, function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                foundUser.secret = submittedSecret;
-                var transporter = nodemailer.createTransport({
-                    service: "gmail",
-                    auth: {
-                        user: "@gmail.com",
-                        pass: "",
-                    },
-                });
+// app.get("/submit", function (req, res) {
+//     if (req.isAuthenticated()) {
+//         res.render("submit");
+//     } else {
+//         res.redirect("/login");
+//     }
+// });
 
-                var mailOptions = {
-                    from: "@gmail.com",
-                    to: foundUser.username,
-                    subject: "LOR Submission",
-                    text: submittedSecret,
-                };
+// app.post("/submit", function (req, res) {
+//     const submittedSecret = req.body.secret;
+//     User.findById(req.user.id, function (err, foundUser) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             if (foundUser) {
+//                 foundUser.secret = submittedSecret;
+//                 var transporter = nodemailer.createTransport({
+//                     service: "gmail",
+//                     auth: {
+//                         user: "@gmail.com",
+//                         pass: "",
+//                     },
+//                 });
 
-                transporter.sendMail(mailOptions, function (err, info) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("Email sent: " + info.response);
-                    }
-                });
-                foundUser.save(function () {
-                    res.redirect("/secrets");
-                });
-            }
-        }
-    });
-});
+//                 var mailOptions = {
+//                     from: "@gmail.com",
+//                     to: foundUser.username,
+//                     subject: "LOR Submission",
+//                     text: submittedSecret,
+//                 };
 
-app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
-});
+//                 transporter.sendMail(mailOptions, function (err, info) {
+//                     if (err) {
+//                         console.log(err);
+//                     } else {
+//                         console.log("Email sent: " + info.response);
+//                     }
+//                 });
+//                 foundUser.save(function () {
+//                     res.redirect("/secrets");
+//                 });
+//             }
+//         }
+//     });
+// });
 
-app.post("/register", function (req, res) {
-    User.register({ username: req.body.username }, req.body.password, function (
-        err,
-        user
-    ) {
-        if (err) {
-            console.log(err);
-            res.redirect("/register");
-        } else {
-            var transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                    user: "@gmail.com",
-                    pass: "",
-                },
-            });
-            var mailOptions = {
-                from: "@gmail.com",
-                to: req.body.username,
-                subject: "LOR Account verification",
-                text: "Thankyou for registering to our official LOR page!",
-            };
+// app.get("/logout", function (req, res) {
+//     req.logout();
+//     res.redirect("/");
+// });
 
-            transporter.sendMail(mailOptions, function (err, info) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Email sent: " + info.response);
-                }
-            });
-            passport.authenticate("local")(req, res, function () {
-                res.redirect("/secrets");
-            });
-        }
-    });
-});
+// app.post("/register", function (req, res) {
+//     User.register({ username: req.body.username }, req.body.password, function (
+//         err,
+//         user
+//     ) {
+//         if (err) {
+//             console.log(err);
+//             res.redirect("/register");
+//         } else {
+//             var transporter = nodemailer.createTransport({
+//                 service: "gmail",
+//                 auth: {
+//                     user: "@gmail.com",
+//                     pass: "",
+//                 },
+//             });
+//             var mailOptions = {
+//                 from: "@gmail.com",
+//                 to: req.body.username,
+//                 subject: "LOR Account verification",
+//                 text: "Thankyou for registering to our official LOR page!",
+//             };
 
-app.post("/login", function (req, res) {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password,
-    });
-    req.login(user, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            passport.authenticate("local")(req, res, function () {
-                res.redirect("/secrets");
-            });
-        }
-    });
-});
+//             transporter.sendMail(mailOptions, function (err, info) {
+//                 if (err) {
+//                     console.log(err);
+//                 } else {
+//                     console.log("Email sent: " + info.response);
+//                 }
+//             });
+//             passport.authenticate("local")(req, res, function () {
+//                 res.redirect("/secrets");
+//             });
+//         }
+//     });
+// });
 
-module.exports = routes;
+// app.post("/login", function (req, res) {
+//     const user = new User({
+//         username: req.body.username,
+//         password: req.body.password,
+//     });
+//     req.login(user, function (err) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             passport.authenticate("local")(req, res, function () {
+//                 res.redirect("/secrets");
+//             });
+//         }
+//     });
+// });
+
+// module.exports = routes;
